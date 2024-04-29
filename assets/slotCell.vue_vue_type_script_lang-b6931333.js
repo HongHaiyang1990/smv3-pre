2538c4bf0ec4d0cf677553c0e2623ebc89ccd087
honghaiyangfulu <honghaiyang@fulu.com>
2024-04-29 15:25:19 +0800

    update

---
+++ assets/slotCell.vue_vue_type_script_lang-b6931333.js
@@ -0,0 +1,5 @@
+import{L as t,cd as l}from"./index-18b660c1.js";/**
+ * tdesign v1.7.1
+ * (c) 2023 tdesign
+ * @license MIT
+ */var e=t(l);const r={name:"SlotCell",props:{cell:{type:[Function,String,Object],default:()=>""}},render(){return this.cell}};export{e as D,r as _};
